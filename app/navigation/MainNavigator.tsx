36077fdc6f4409beede7aa09ec5b8680fd3f6c1d
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2024-02-27 10:00:21 -0800

    SAFE-117-App: Resulve redundancy on Navigation [frontend]

--- app/navigation/MainNavigator.tsx
+++ app/navigation/MainNavigator.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import LoginScreen from "../screens/Login";
 import MainTabNavigator from "./MainTabNavigator";
+import LoginScreen from "../screens/Login";
 
 const Stack = createStackNavigator();
 
 const MainNavigator: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Main" component={MainTabNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Main" component={MainTabNavigator} />
+    </Stack.Navigator>
   );
 };
 
