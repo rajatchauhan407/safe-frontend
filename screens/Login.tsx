20c0f8ff45278f991b6ba1020d9284835e7b3d90
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2024-03-03 00:26:50 -0800

    SAFE-145-App: Login screen components and routing to user dashboards

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,18 +1,36 @@
-import React from "react";
-import { View, Text,TouchableOpacity, StyleSheet} from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { RootStackParamList } from "../types/navigationTypes";
-import { NavigationProp } from '@react-navigation/native';
+import { NavigationProp } from "@react-navigation/native";
+
+import Dropdown from "../components/common/Dropdown";
 
 const LoginScreen: React.FC = () => {
+  const [loginAs, setLoginAs] = useState<string>("");
+  const [workerID, setWorkerID] = useState<string>("");
+  const [supervisorID, setSupervisorID] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
-  const handleSupervisorLogin = () => {
-    navigation.navigate('Main', { screen: 'Supervisor', params: { screen: 'Dashboard' } });
-  };
-
-  const handleWorkerLogin = () => {
-    navigation.navigate('Main', { screen: 'Worker', params: { screen: 'Dashboard' } });
+  const handleLogin = () => {
+    if (loginAs === "Supervisor") {
+      navigation.navigate("Main", {
+        screen: "Supervisor",
+        params: { screen: "Dashboard" },
+      });
+    } else if (loginAs === "Worker") {
+      navigation.navigate("Main", {
+        screen: "Worker",
+        params: { screen: "Dashboard" },
+      });
+    }
   };
 
   return (
@@ -22,18 +40,48 @@ const LoginScreen: React.FC = () => {
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           style={styles.button}
-          onPress={handleSupervisorLogin}
+          onPress={() => setLoginAs("Supervisor")}
         >
-          <Text style={styles.buttonText}>Supervisor Dashboard</Text>
+          <Text style={styles.buttonText}>Supervisor</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.button}
-          onPress={handleWorkerLogin}
+          onPress={() => setLoginAs("Worker")}
         >
-          <Text style={styles.buttonText}>Worker Dashboard</Text>
+          <Text style={styles.buttonText}>Worker</Text>
         </TouchableOpacity>
       </View>
+
+      <Text style={styles.label}>Site</Text>
+      <Dropdown />
+
+      <Text style={styles.label}>
+        {loginAs === "Worker" ? "Worker" : "Supervisor"} ID
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter ID"
+        onChangeText={(text) =>
+          loginAs === "Worker" ? setWorkerID(text) : setSupervisorID(text)
+        }
+        value={loginAs === "Worker" ? workerID : supervisorID}
+        autoCapitalize="none"
+        keyboardType="numeric"
+      />
+
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Password"
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -41,28 +89,65 @@ const LoginScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#F8F8FF",
+    padding: 20,
   },
   text: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 20,
   },
   buttonContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
   },
   button: {
-    backgroundColor: '#f4511e',
+    backgroundColor: "#FD9201",
     padding: 15,
     borderRadius: 5,
-    margin: 10,
+    width: "48%",
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  loginButton: {
+    backgroundColor: "#FD9201",
+    padding: 15,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  loginButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  activeButton: {
+    backgroundColor: "blue",
+    borderColor: "blue",
+    color: "white",
   },
 });
+
 export default LoginScreen;
