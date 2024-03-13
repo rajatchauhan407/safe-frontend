dc2db0c832f2abd78883fbb05dfaef1552c442e6
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2024-03-12 17:35:36 -0700

    SAFE-211-Supervisor: Checked Id List layout ready to connect to backend. Styling missing.

--- screens/supervisor/CheckedIn.tsx
+++ screens/supervisor/CheckedIn.tsx
@@ -1,12 +1,52 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { Box, ScrollView, Text } from "@gluestack-ui/themed";
+import ScreenLayout from "../../components/layout/screenLayout";
+import NumOfWorkers from "../../components/common/NumOfWorkers";
+import Drawer from "../../components/common/Drawer";
+import CheckedInList from "../../components/supervisor/CheckedInList";
 
 const CheckedIn: React.FC = () => {
+  const [siteLocation, setSiteLocation] = useState("Richmond, BC");
+  const [currentAlertText, setCurrentAlertText] = useState(
+    "Great! There’s no alert report."
+  );
+
+  /* Use this for alert texts different than default */
+  /* useEffect(() => {
+    setCurrentAlertText("Hi");
+  }, []); */
+
   return (
-    <View>
-      <Text>ChechedId Screen Supervisor</Text>
-    </View>
+    <ScreenLayout>
+      <ScrollView>
+        {/* LOCATION */}
+        <Box mx="$3" mb="$3">
+          {/* Icon missing here */}
+          <Text>{siteLocation}</Text>
+        </Box>
+
+        {/* WORKERS CHECKED IN */}
+        <NumOfWorkers totalCheckedIn={30} totalExpected={34} />
+
+        {/* LIST OF WORKERS */}
+        <CheckedInList />
+      </ScrollView>
+      <Box style={styles.drawer}>
+        <Drawer alertText={currentAlertText} />
+      </Box>
+    </ScreenLayout>
   );
 };
+
+const styles = StyleSheet.create({
+  drawer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 999,
+  },
+});
 
 export default CheckedIn;
