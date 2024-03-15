60d265b0f547f6dde55b895153d1d70d85dbab26
cindycharvet <cindycharvetweb@gmail.com>
2024-03-15 01:36:02 -0700

    finish styling the checkout button

--- screens/worker/Profile.tsx
+++ screens/worker/Profile.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { View, Text } from "react-native";
+import ScreenLayout from "../../components/layout/screenLayout";
 import AlertMessage from "../../components/common/alertMessage";
 
 const Profile: React.FC = () => {
   return (
+  <>  
+  <AlertMessage backgroundColor="#00AE8C" text="Your incident has been reported" />
+  <ScreenLayout>
     <View>
-      <AlertMessage backgroundColor="#00AE8C" text="Your incident has been reported" />
+      <Text>Profile</Text>
     </View>
+  </ScreenLayout>
+  </>
   );
 };
 
