77bc384f41bd9c5d74c2cbba60fa55e6490cfb52
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2024-03-15 18:52:25 -0700

    SAFE-278-Supervisor: Checked in workers card styled

--- components/common/NumOfWorkers.tsx
+++ components/common/NumOfWorkers.tsx
@@ -10,6 +10,8 @@ import {
 } from "@gluestack-ui/themed";
 import { StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import Typography from "./typography";
+import CommonButton from "./button";
 
 interface NumOfWorkersProps {
   totalCheckedIn: number;
@@ -27,36 +29,38 @@ const NumOfWorkers: React.FC<NumOfWorkersProps> = ({
   };
 
   return (
-    <Card size="md" variant="filled" bgColor="$highlight" borderRadius="$3xl">
+    <Card size="md" variant="elevated" bgColor="$highlight" borderRadius="$3xl">
       <Heading mb="$1" size="md">
         Total Checked-in Workers
       </Heading>
       <HStack sx={{ justifyContent: "space-between", alignItems: "center" }}>
-        <HStack>
-          <Text>{totalCheckedIn}</Text>
-          <Text>/</Text>
-          <Text>{totalExpected}</Text>
+        <HStack alignItems="center">
+          <Typography style={styles.countIn}>{totalCheckedIn}</Typography>
+          <Typography style={styles.countExpected}> / </Typography>
+          <Typography style={styles.countExpected}>{totalExpected}</Typography>
         </HStack>
 
-        <Button
-          size="md"
-          variant="link"
-          action="primary"
-          onPress={handleSeeAll}
-        >
-          <ButtonText sx={{ textDecorationLine: "underline" }}>
+        <CommonButton variant="underline" onPress={handleSeeAll}>
+          <Typography bold style={styles.linkBnt}>
             See All
-          </ButtonText>
-        </Button>
+          </Typography>
+        </CommonButton>
       </HStack>
     </Card>
   );
 };
 
 const styles = StyleSheet.create({
-  count: {
+  countIn: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  countExpected: {
     fontSize: 22,
     fontWeight: "bold",
+  },
+  linkBnt: {
+    fontSize: 20,
   },
 });
 
