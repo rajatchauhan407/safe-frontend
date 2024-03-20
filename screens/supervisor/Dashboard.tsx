8623a4f56bd958c28d7c6536f0c6ccbb5a97877c
MagdelineJannet <janet.magdeline@gmail.com>
2024-03-20 12:49:24 -0700

    handled worker checkout during logout and got site location from backend for worker and supervisor dashboard

--- screens/supervisor/Dashboard.tsx
+++ screens/supervisor/Dashboard.tsx
@@ -14,10 +14,11 @@ import LocationComponent from "../../components/supervisor/Location";
 import SafeZoneWorkers from "../../components/common/safeZoneWorkers";
 import { useSelector} from "react-redux";
 import { RootState} from "../../lib/store";
+import { BACKEND_BASE_URL } from "../../config/api";
 
 const Dashboard: React.FC = () => {
   // const [userName, setUserName] = useState("David");
-  const [siteLocation, setSiteLocation] = useState("Richmond, BC");
+  const [siteLocation, setSiteLocation] = useState("");
   const [currentAlertType, setCurrentAlertType] = useState<
     "none" | "accident" | "evacuation" | "sos"
   >("none");
@@ -26,10 +27,41 @@ const Dashboard: React.FC = () => {
     (state: RootState) => state.auth
   );
   let userName = "";
+  let siteId="";
   if (user) {
+    siteId = user.constructionSiteId || "";
     console.log("logged in user>> " + user._id);   
     userName = `${user.firstName} ${user.lastName}`;
   } 
+
+  useEffect(() => {
+      const getSite = async () => {
+      try {
+        const siteInfo = {
+          siteId : siteId
+        }
+        const res = await fetch(`${BACKEND_BASE_URL}/sitename`, {
+          method: "POST",
+          credentials: 'include',
+          body: JSON.stringify(siteInfo),
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+        const data = await res.json();
+        console.log("site name data>> "+data)
+        if (data) {
+          setSiteLocation(data)    
+        } 
+      } 
+      catch (error) {
+        //Error while connecting with backend
+        console.error('Error:', error);
+      }
+    };
+
+    getSite();
+  }, []);
 
   useEffect(() => {
     websocketService.connect();
